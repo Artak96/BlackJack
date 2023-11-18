171bc080ef2abdb73f70a6297fa139007000c732
Artak96 <art.amiryan96@gmail.com>
2023-11-18 16:25:10 +0400

    Add project files.

---
+++ BlackJack.DAL/Context/BlackJackDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.DAL.Context
+{
+    public class BlackJackDbContext : DbContext
+    {
+        public BlackJackDbContext(DbContextOptions<BlackJackDbContext> options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
